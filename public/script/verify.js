e247b5593518e655a561f5c799ffb04dcc3270b5
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2025-11-23 13:11:17 -0300

    update

---
+++ public/script/verify.js
@@ -0,0 +1,53 @@
+const email = new URLSearchParams(window.location.search).get("email") // pega o email da URL
+
+document.querySelector("#emailSpan").textContent = email
+
+//botão verificar
+const btnVerify = document.querySelector("#btnVerify")
+
+btnVerify.addEventListener("click", async ()=>{
+    const code = document.querySelector("#codeInput").value
+
+    const res = await fetch("https://glammis.onrender.com/auth/verify", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ email, code})
+    })
+
+    const data = await res.json()
+
+    if( res.ok) {
+        alert("Verificado com sucesso!")
+        window.location.href = "/dashboard.html"
+    } else {
+        alert(data.message)
+    }
+})
+
+//cronômetro de 30s para pedir reenvio de código
+let seconds = 30
+const timerEl = document.querySelector("#timer")
+const resendBtn = document.querySelector("#btnResend")
+
+const interval = setInterval(()=>{
+    seconds--
+    timerEl.textContent = seconds
+
+    if (seconds <= 0) {
+        clearInterval(interval)
+        resendBtn.classList.replace( "inative-resend", "active-resend")
+    }
+}, 1000)
+
+//Reenviar código
+resendBtn.addEventListener("click", async ()=>{
+    const res = await fetch("https://glammis.onrender.com/auth/resend-code", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email})
+    })
+
+    const data = await res.json()
+    alert(data.message)
+    resendBtn.classList.replace( "active-resend", "inative-resend")
+})
