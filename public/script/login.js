716f9658e1902a02bd4dbc9704d6c14200c1a526
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2025-11-23 03:09:25 -0300

    update

--- public/script/login.js
+++ public/script/login.js
@@ -32,7 +32,7 @@ btnLogin.addEventListener("click", async () => {
     };
 
     try {
-        const response = await fetch("http://localhost:8081/auth/login", {
+        const response = await fetch("https://glammis.onrender.com/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
