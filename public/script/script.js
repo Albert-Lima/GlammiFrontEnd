4cf06fc3abee44263a9c7188e810dfb13f4cd2b1
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2026-03-08 00:50:50 -0300

    update

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,18 @@
 const navMain = document.querySelector(".nav-main")
 const btnMenu = document.querySelector("#btnMenu")
+const imgButtonMenu = document.querySelector(".imgButtonMenu")
 const navigate = document.querySelector(".navigate")
 
 btnMenu.addEventListener("click", ()=>{
 
     if(navigate.classList.contains("inactive-link-menu")){
+
+        btnMenu.style.position = "absolute"
+        btnMenu.style.right = "10px"
+        btnMenu.style.top = "10px"
+
+        imgButtonMenu.src = ""
+        imgButtonMenu.src = "public/img/icons8-close-64.png"
 
         navMain.classList.remove("inactive-menu")
         navMain.classList.add("active-menu")
@@ -13,6 +21,11 @@ btnMenu.addEventListener("click", ()=>{
         navigate.classList.add("active-link-menu")
 
     } else if(navigate.classList.contains("active-link-menu")){
+
+        btnMenu.removeAttribute("style")
+
+        imgButtonMenu.src = ""
+        imgButtonMenu.src = "public/img/icons8-menu-64.png"
 
         navMain.classList.remove("active-menu")
         navMain.classList.add("inactive-menu")
